4101c664c5bdf4802a9b8132f13a40f2494b6f1b
Milosz Banaszak <milosz.banaszak94@gmail.com>
2025-02-15 19:24:52 +0000

    Adding matches

--- DartsTracker/Models/Match.cs
+++ DartsTracker/Models/Match.cs
@@ -2,5 +2,12 @@
 {
     public class Match
     {
+        public int Id { get; set; }
+        public int TournamentId { get; set; }
+        public Tournament Tournament { get; set; } = null!;
+        public bool IsWin { get; set; }
+        public int DartsThrown { get; set; }
+        public double MatchAverage { get; set; }
+        public double FirstNineDartAverage { get; set; }
     }
 }
